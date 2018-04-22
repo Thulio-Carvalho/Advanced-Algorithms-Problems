73f1225e5c1d6585a5810d264ca8159e7be18e5c
Thulio Carvalho <thulioicc@gmail.com>
2018-04-22 16:56:56 -0300

    added finished B problem code

--- 2 - Segtree and Lazy Propagation/B.cpp
+++ 2 - Segtree and Lazy Propagation/B.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 typedef long long ll;
 
-int T, N, C, arr[MAXN];
+int T, N, C;
 
 ll tree[4*MAXN], lazy[4*MAXN];
 
@@ -13,33 +13,23 @@ ll mergeValues(ll x, ll y){
   return x + y;
 }
 
-void build(int id, int l, int r){
-  if(l == r){
-    tree[id] = arr[l];
-    return;
-  }
-  int mid = (l + r) / 2;
-  build(id*2, l, mid);
-  build(id*2 - 1, mid + 1, r);
-  tree[id] = mergeValues(tree[id*2], tree[id*2]);
-}
-
-void update(int id, int l, int r, int x, int y, int val){
+void doLazy(int id, int l, int r){
   if (lazy[id] != 0){
-    tree[id] += lazy[id] * (r-l+1);
+    tree[id] += lazy[id] * (r - l + 1);
     if (l != r){
-      lazy[tree[id*2]] += lazy[id];
-      lazy[tree[id*2+1]] += lazy[id];
+      lazy[id*2] += lazy[id];
+      lazy[id*2+1] += lazy[id];
     }
     lazy[id] = 0;
   }
+}
+
+void update(int id, int l, int r, int x, int y, ll val){
+  doLazy(id, l, r);
   if (x > r || y < l) return;
   if (x <= l && y >= r){
-    tree[id] += val * (r - l +1);
-    if(l != r){
-      lazy[id*2] += val;
-      lazy[id*2+1] += val;
-    }
+    lazy[id] += val;
+    doLazy(id, l, r);
     return;
   }
   int mid = (l + r) / 2;
@@ -49,15 +39,8 @@ void update(int id, int l, int r, int x, int y, int val){
 }
 
 ll query(int id, int l, int r, int x, int y){
+  doLazy(id, l, r);
   if (x > r || y < l) return 0;
-  if (lazy[id] != 0){
-    tree[id] += lazy[id] * (r -l + 1);
-    if(l != r){
-      lazy[id*2] += lazy[id];
-      lazy[id*2+1] += lazy[id];
-    }
-    lazy[id] = 0;
-  }
   if (x <= l && y >= r) return tree[id];
   int mid = (l + r) / 2;
   return mergeValues(query(id*2, l, mid, x, y),
@@ -70,16 +53,16 @@ int main(){
     scanf("%d %d", &N, &C);
     memset(lazy, 0, sizeof lazy);
     memset(tree, 0, sizeof tree);
-    build(1, 1, N);
     while (C--){
-      int type, p, q, v;
+      int type, p, q;
+      ll v;
       scanf("%d", &type);
       if(type == 0){
-        scanf("%d %d %d", &p, &q, &v);
-        update(1, 1, N, p, q, v);                   // TODO code function update
+        scanf("%d %d %lld", &p, &q, &v);
+        update(1, 1, N, p, q, v);
       } else{
         scanf("%d %d", &p, &q);
-        ll ans = query(1, 1, N, p, q);              // TODO code function query
+        ll ans = query(1, 1, N, p, q);
         printf("%lld\n", ans);
       }
     }
