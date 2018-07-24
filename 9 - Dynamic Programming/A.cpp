12473508e94456f8bd2972631e4a0ecbd7bca0d8
Thulio Carvalho <thulioicc@gmail.com>
2018-07-24 09:09:26 -0300

    added dynamic programming folder

---
+++ 9 - Dynamic Programming/A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define debug(arr, len) cerr << #arr << " = "; for (int i=0;i<len;i++)cout << arr[i] << " \n"[i == n-1]
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    return 0;
+}
