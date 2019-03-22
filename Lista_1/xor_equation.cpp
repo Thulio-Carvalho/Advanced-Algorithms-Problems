8cce8b06388ccb494a08cc0f6b8ff9c0cbc0e18a
Thúlio <thulioicc@gmail.com>
2019-03-22 14:24:43 -0300

    changes to xor_equation

--- Lista_1/xor_equation.cpp
+++ Lista_1/xor_equation.cpp
@@ -1,31 +1,53 @@
 #include <bits/stdc++.h>
-#define MAXN 40
+#define MAXN 61
+
 using namespace std;
 
-int S, X;
-int dp[MAXN][2];
+typedef long long ll;
+
+ll S, X;
+ll dp[MAXN][2];
+int limit;
 
 bool bit(int S, int k) {
-    return (S & ( 1 << S )) >> k;
+    return (S & ( 1 << k ));
 }
 
-int solve(int i, bool carry){
+ll solve(int i, bool carry){
     if (dp[i][carry] != -1) {
         return dp[i][carry];
     }
     if (bit(X, i) && bit(S, i) && carry) return 0;
     if (bit(X, i) && !bit(S, i) && !carry) return 0;
-    if (!bit(X, i) && bit(S, i) && !carry) return 0;
     if (!bit(X, i) && !bit(S, i) && carry) return 0;
+    if (!bit(X, i) && bit(S, i) && !carry) return 0;
 
-    // if (bit(X, i) && bit(S, i) && !carry){
-    //     dp[]
-    // }
+    if (i >= limit) return !carry;
 
+    if (bit(X, i) && bit(S, i) && !carry){
+        return dp[i][carry] = 2 * solve(i+1, 0);
+    }
+    if (bit(X, i) && !bit(S, i) && carry){
+        return dp[i][carry] = 2 * solve(i+1, 1);
+    }
+    if (!bit(X, i) && bit(S, i) && carry){
+        return dp[i][carry] = solve(i+1, 0) + solve(i+1, 1);
+    }
+    if (!bit(X, i) && !bit(S, i) && !carry){
+        return dp[i][carry] = solve(i+1, 1) + solve(i+1, 0);
+    }
 }
 
 int main(){
     cin >> S >> X;
+    
+    for(int i = 0; i < MAXN; i++) {
+        dp[i][0] = -1;
+        dp[i][1] = -1;
+    }
 
+    limit = 61;
+
+    cout << solve(0, 0) << endl;    
     return 0;
 }
