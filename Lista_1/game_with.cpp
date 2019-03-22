7e69aac88ebbf5c5c2b0311b8a2becfb67727d4c
Thúlio <thulioicc@gmail.com>
2019-03-21 23:40:25 -0300

    added game_with_string code

--- Lista_1/game_with.cpp
+++ Lista_1/game_with.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
-int main(){
+string word;
 
+int main(){
+    cin >> word; 
+
+    stack <char> removed;
+    bool win = false;
+
+    for (char l : word) {
+        if (removed.size() > 0) {
+            if (removed.top() == l) {
+                removed.pop();
+                win = !win;
+            } else {
+                removed.push(l);
+            }
+        } else {
+            removed.push(l);
+        }
+    }
+
+    if (win) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
 
     return 0;
 }
